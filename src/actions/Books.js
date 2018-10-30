f229a2bfd94e16b1d5d6a90ec887254f1b73c5b9
nerijus-ubuntu <nerijusanz@gmail.com>
2018-10-30 12:51:54 +0200

    created Book.js file actions

---
+++ src/actions/Books.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import api from '../api/api';
+
+export const books = () => (dispatch) => {}
+
+export const addBook = () => (dispatch) => {}
+
+export const searchBook = (query) => (dispatch) => {
+/*
+    dispatch(authLoading(true));
+
+    api.auth.signup(data)
+        .then(()=>{
+
+            dispatch(authSignup(true));
+
+        })
+        .catch(err=>{
+
+            dispatch(authSignup(false));
+
+            dispatch(authError(err.response.data.errors));
+
+        })
+*/
+}
