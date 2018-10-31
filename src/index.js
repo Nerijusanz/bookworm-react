1f5915d2d97c7e34a2a162bbd45b88f2c9dcc992
nerijus-ubuntu <nerijusanz@gmail.com>
2018-10-31 14:08:37 +0200

    index.js, update import provider link goes  up to top imports.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import dotenv from 'dotenv';
 import 'semantic-ui-css/semantic.min.css';
-
-import {Provider} from 'react-redux';
 
 import store from './store';    // REDUX STORE;
 import App from './App';
