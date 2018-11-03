3256618872cb201e02b67c8a47d2e82743071828
nerijus-ubuntu <nerijusanz@gmail.com>
2018-11-03 15:07:38 +0200

    created new userbookReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import auth from './authReducer';
 import book from './bookReducer';
+import userbook from './userbookReducer';
 import flashMessage from './flashReducer';
 
 export default combineReducers({
     auth,
     book,
+    userbook,
     flashMessage
 })
---
+++ src/reducers/userbookReducer.js
@@ -0,0 +1,15 @@
+import { 
+
+} from '../actions/types';
+
+const initialState={
+    books:[]
+};
+
+export default function userbook(state=initialState,action){
+    switch(action.type){
+        
+
+        default:return state;
+    }
+} 
