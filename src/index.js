947772d28d119728315459c0306392edc6c6957e
nerijus-ubuntu <nerijusanz@gmail.com>
2018-11-19 16:49:59 +0200

    update localstorage REACT_APP_LOCAL_STORAGE_TOKEN loading from .env file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import dotenv from 'dotenv';
+
 import 'semantic-ui-css/semantic.min.css';
 
 import store from './store';    // REDUX STORE;
@@ -11,12 +11,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 import {setAuthorizationHeader} from './actions/Auth';
 
-// -------------initialize env variables---------
-dotenv.config();
-// ----------------------------------------------
 
-if(localStorage.getItem('bookwormUserToken'))
-    setAuthorizationHeader(localStorage.getItem('bookwormUserToken'));
+if(localStorage.getItem(process.env.REACT_APP_LOCAL_STORAGE_TOKEN))
+    setAuthorizationHeader(localStorage.getItem(process.env.REACT_APP_LOCAL_STORAGE_TOKEN));
 
 
 ReactDOM.render(
