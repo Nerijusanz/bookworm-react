f7660e5b55b8b477a7092d3eb730b5d91a2c2af4
nerijus-ubuntu <nerijusanz@gmail.com>
2019-01-09 14:30:20 +0200

    LoginPage updated. removed isAuthenticated prop

--- src/components/pages/auth/LoginPage.js
+++ src/components/pages/auth/LoginPage.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import propTypes from 'prop-types';
-import {connect} from 'react-redux';
 import LoginForm from '../../forms/auth/LoginForm';
 
 
-const LoginPage = ({isAuthenticatedToken}) => (
+const LoginPage = () => (
   <div>
     <h1>LoginPage</h1>
-    {isAuthenticatedToken ? this.context.router.history.push('/dashboard') : <LoginForm />}
+     <LoginForm />
   </div>
 );
 
-LoginPage.propTypes = {
-  
-  isAuthenticatedToken: propTypes.bool.isRequired
+LoginPage.propTypes = {}
 
-}
-
-LoginPage.contextTypes = {
-  router: propTypes.object.isRequired
-}
-
-
-function mapStateToProps(state){
-  return{
-    isAuthenticatedToken: !!state.auth.token
-  }
-}
-
-
-export default connect(mapStateToProps,{})(LoginPage);
+export default (LoginPage);
