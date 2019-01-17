42600485bf4edbb795c1d2d2f4b80fbbf124cc27
nerijus-ubuntu <nerijusanz@gmail.com>
2019-01-17 13:23:36 +0200

    HomePage.js cleanup

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,43 +1,9 @@
-import React, { Component } from 'react';
-import propTypes from 'prop-types';
-import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
-import {logout} from '../../actions/Auth';
+import React from 'react';
 
+const HomePage = () => (
+    <div>
+        <h1>HomePage</h1>
+    </div>
+);
 
-class HomePage extends Component {
-
-    state
-
-    render() {
-      return (
-        <div>
-            <h1>HomePage</h1>
-            {
-            this.props.isAuthenticated ? 
-                <button onClick={()=>this.props.logout()}>logout</button> : 
-                
-                <p><Link to="/login">Login</Link>
-                <span>or</span>
-                <Link to="/signup">signup</Link></p>
-            }
-        </div>
-      )
-    }
-  }
-
-
-HomePage.propTypes={
-    isAuthenticated: propTypes.bool.isRequired,
-    logout: propTypes.func.isRequired,
-
-}
-
-
-function mapStateToProps(state){
-    return{
-        isAuthenticated: !!state.auth.token
-    }
-}
-
-export default connect(mapStateToProps,{logout})(HomePage);
+export default HomePage;
