baa2282920dfc8f0d7a4a392e4c05c260cfce0ed
nerijus-ubuntu <nerijusanz@gmail.com>
2019-01-18 14:43:34 +0200

    ServerErrorItem.js updated

--- src/components/messages/ServerErrorItem.js
+++ src/components/messages/ServerErrorItem.js
@@ -2,12 +2,11 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 
-const ServerErrorItem = ({key,item}) => 
-    <li key={key}>{item}</li>
+const ServerErrorItem = ({item}) => <li>{item}</li>
 
 
 ServerErrorItem.propTypes = {
-    key: propTypes.string.isRequired,
+
     item: propTypes.string.isRequired
 }
 
