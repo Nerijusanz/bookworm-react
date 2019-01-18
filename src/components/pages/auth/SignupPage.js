d545fc65faf639fe470889c10e415b2b60d2aaa1
nerijus-ubuntu <nerijusanz@gmail.com>
2019-01-18 17:52:02 +0200

    SignupPage.js structure changed

--- src/components/pages/auth/SignupPage.js
+++ src/components/pages/auth/SignupPage.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import {connect} from 'react-redux';
 
+import {signupEmailExists,signup} from '../../../actions/Auth';
+
+import ServerError from '../../messages/ServerError';
+
 import SignupForm from '../../forms/auth/SignupForm';
 import InlineMessage from '../../messages/InlineMessage';
 
+
+
 class SignupPage extends Component {
 
+  signup = (data) => {
+
+    this.props.signup(data);
+
+  }
+
+  signupEmailExists = (email) => {
+
+    this.props.signupEmailExists(email);
+
+  }
 
   messageInfo = () =>
 
@@ -16,15 +33,19 @@ class SignupPage extends Component {
 
   render() {
     // ----------------state variables ------------------------
-    const {success} = this.props.auth; // redux: auth reducer
+    const {auth} = this.props; // redux: auth reducer
     // --------------------------------------------------------
 
-    const content = success ? this.messageInfo() : <SignupForm />
+    const content = auth.success ? this.messageInfo() : <SignupForm auth={auth} signupEmailExists={this.signupEmailExists} signup={this.signup} />
          
 
     return (
-      <div>
-        <h1>Sign up Page</h1>
+      <div className="page signup-page">
+
+        <h1 className="page-title">Sign up Page</h1>
+
+          { auth.serverErrors.global && <ServerError errors={auth.serverErrors.global} /> }
+          { auth.serverErrors.signup_email && <ServerError errors={auth.serverErrors.signup_email} /> }
 
           {content}
             
@@ -34,12 +55,13 @@ class SignupPage extends Component {
 }
 
 SignupPage.propTypes = {
+
   auth: propTypes.shape({
-
       success: propTypes.bool.isRequired,
-
   }).isRequired,
 
+  signupEmailExists: propTypes.func.isRequired,
+  signup: propTypes.func.isRequired,
 }
 
 
@@ -51,4 +73,4 @@ function mapStateToProps(state){
 }
 
 
-export default connect(mapStateToProps,{})(SignupPage);
+export default connect(mapStateToProps,{signupEmailExists,signup})(SignupPage);
