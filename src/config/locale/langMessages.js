fe779549de83125075e3006e3116f8c06c12b224
nerijus-ubuntu <nerijusanz@gmail.com>
2019-01-18 16:02:07 +0200

    langMessages.js updated

--- src/config/locale/langMessages.js
+++ src/config/locale/langMessages.js
@@ -2,6 +2,7 @@ export default {
     en:{
         nav_item_brand_dashboard:'Dashboard',
         nav_item_brand_main:'Main',
+        nav_item_home:'Home',
         nav_item_signup:'Signup',
         nav_item_login:'Login',
         nav_item_mybooks:'My Books',
@@ -10,16 +11,26 @@ export default {
 
         page_home_page_title:'Home',
 
+        page_login_page_title:'Login',
+        page_login_form_input_email_label:'Email',
+        page_login_form_input_email_placeholder:'email@email.com',
+        page_login_form_input_password_label:'Password',
+        page_login_form_button_login:'login',
+        page_login_form_link_forgot_password:'forgot password',
+        page_login_form_link_signup:'sign up',
+
+
         page_forgotpassword_page_title:'Forgot Password',
         page_forgotpassword_form_input_label_email:'Email',
         page_forgotpassword_form_input_placeholder_email:'email@email.com',
         page_forgotpassword_form_button_reset_password:'Reset password',
 
-        page_login_page_title:'Login',
+        
     },
     ru:{
         nav_item_brand_dashboard:'приборная доска',
         nav_item_brand_main:'Главный',
+        nav_item_home:'Главный',
         nav_item_signup:'регистрационная',
         nav_item_login:'войти',
         nav_item_mybooks:'мои книги',
@@ -28,17 +39,26 @@ export default {
 
         page_home_page_title:'Главный',
 
+        page_login_page_title:'Авторизоваться',
+        page_login_form_input_email_label:'электронная почта',
+        page_login_form_input_email_placeholder:'почта@почта.com',
+        page_login_form_input_password_label:'пароль',
+        page_login_form_button_login:'войти',
+        page_login_form_link_forgot_password:'Забыли пароль',
+        page_login_form_link_signup:'зарегистрироваться',
+
         page_forgotpassword_page_title:'Забыли пароль',
         page_forgotpassword_form_input_label_email:'Электронная почта',
         page_forgotpassword_form_input_placeholder_email:'почта@почта.com',
         page_forgotpassword_form_button_reset_password:'Сброс пароля',
 
-        page_login_page_title:'Авторизоваться',
+        
     },
     lt:{
         nav_item_brand_dashboard:'Valdymo skydelis',
         nav_item_brand_main:'Pagrindinis',
         nav_item_signup:'Registracija',
+        nav_item_home:'Pagrindinis',
         nav_item_login:'Prisijungti',
         nav_item_mybooks:'Mano knygos',
         nav_item_booksapi:'Knygos API',
@@ -46,11 +66,19 @@ export default {
 
         page_home_page_title:'Pagrindinis',
 
+        page_login_page_title:'Prisijungti',
+        page_login_form_input_email_label:'El. paštas',
+        page_login_form_input_email_placeholder:'el.paštas@paštas.com',
+        page_login_form_input_password_label:'Slaptažodis',
+        page_login_form_button_login:'Prisijungti',
+        page_login_form_link_forgot_password:'Pamiršote slaptažodį',
+        page_login_form_link_signup:'Prisiregistruoti',
+
         page_forgotpassword_page_title:'Pamiršote slaptažodį',
         page_forgotpassword_form_input_label_email:'El. paštas',
         page_forgotpassword_form_input_placeholder_email:'paštas@paštas.com',
         page_forgotpassword_form_button_reset_password:'Atstatyti slaptažodį',
 
-        page_login_page_title:'Prisijungti',
+
     }
 }
